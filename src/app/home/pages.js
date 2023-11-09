04864c668a2bfbd313f42b98413716d5de6f0c6b
Arshad Khan <developer.codewitharsh@gmail.com>
2023-11-09 18:03:42 +0530

    first commit

---
+++ src/app/home/pages.js
@@ -0,0 +1,222 @@
+"use client";
+import { Box, Container, Grid } from "@mui/material";
+import React from "react";
+import styles from "./Home.module.css";
+import StarRateIcon from "@mui/icons-material/StarRate";
+import AddToPhotosIcon from "@mui/icons-material/AddToPhotos";
+import Link from "next/link";
+import { BrowseGalleryRounded } from "@mui/icons-material";
+const Home = () => {
+  return (
+    <>
+      {/*  1 Wrapper */}
+      <Box component={"section"} className={styles.home_wrp}>
+        <Container>
+          <Grid container spacing={2} alignItems={"center"}>
+            <Grid item lg={7} md={12} sm={12}>
+              <Box className={styles.left_sec}>
+                <h1 className="aos-animate" data-aos="zoom-in">
+                  your go-to source to <span>manage payment</span>{" "}
+                  <i>with Complete Ease.</i>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et
+                </p>
+
+                <Box className={styles.ftr_btn}>
+                  <a href="#">Get Started Now!</a>
+                  <Box className={styles.active_user}>
+                    <img
+                      src="https://advanture.icu/bden/wp-content/uploads/sites/18/2023/04/newclients.png"
+                      alt=""
+                    />
+                    <p>25k+ Active Users</p>
+                  </Box>
+                </Box>
+              </Box>
+            </Grid>
+            <Grid item lg={5} md={12} sm={12}>
+              <Box className={styles.right_img}>
+                <img
+                  src="https://advanture.icu/bden/wp-content/uploads/sites/18/2023/04/MobileApp03-843x1536.png"
+                  alt=""
+                />
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      {/* servives 2nd Wrapper */}
+      <Box component={"section"} className={styles.home_wrp_service}>
+        <Container className={styles.fx_cnt}>
+          <Grid container spacing={2}>
+            <Grid item lg={12} md={12} sm={12}>
+              <Box className={styles.our_services_heading_name}>
+                <h5>
+                  Our Services{" "}
+                  <sup>
+                    <img
+                      src="https://cook-web-app.netlify.app/images/icons/benefitstik.svg"
+                      alt=""
+                    />
+                  </sup>
+                </h5>
+              </Box>
+              <Box className={styles.our_services}>
+                <div className={styles.service_item}>
+                  <img
+                    src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg"
+                    alt=""
+                  />
+                  <Link href="">Mobile Recharge & DTH</Link>
+                </div>
+                <div className={styles.service_item}>
+                  <img
+                    src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg"
+                    alt=""
+                  />
+                  <Link href="">Bill Payments</Link>
+                </div>
+                <div className={styles.service_item}>
+                  <img
+                    src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg"
+                    alt=""
+                  />
+                  <Link href="">Travel Booking Payments</Link>
+                </div>
+
+                <div className={styles.service_item}>
+                  <img
+                    src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg"
+                    alt=""
+                  />
+                  <Link href="">More</Link>
+                </div>
+                <div className={styles.service_item}>
+                  {/* <img src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg" alt="" />
+                    <Link href="">More</Link> */}
+                </div>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+      {/* reveiw sec 3 Wrapper  */}
+      {true && (
+        <Box component={"section"} className={styles.home_wrp_bag1}>
+          <Container>
+            <Grid container spacing={2} alignItems={"center"}>
+              <div className={styles.shadow_img}>
+                <img
+                  src="https://advanture.icu/bden/wp-content/uploads/sites/18/2023/04/IMG-bg.png"
+                  alt=""
+                />
+              </div>
+              {/* <Grid item lg={7} md={12} sm={12}>
+              <div className={styles.review_sec}>
+                {[...Array(5).keys()].map((_, index) => (
+                  <StarRateIcon key={index} />
+                ))}
+                <h3>
+                  <span>300+</span>
+                  <sub> Reviews</sub>
+                </h3>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit</p>
+              </div>
+            </Grid> */}
+              <Grid item lg={7} md={12} sm={12}>
+                <div className={styles.left_text_bag2}>
+                  <h3>
+                    We Strive To Offer An <span>Unparalleled Experience</span>
+                  </h3>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                    do eiusmod tempor incididunt ut labore et
+                  </p>
+                  <p>
+                    We believe that everyone should have access to financial
+                    services regardless of their background. That’s why we
+                  </p>
+                </div>
+
+                <Grid container>
+                  <Grid lg={6} md={12} sm={12}>
+                    <Box className={styles.items_sec}>
+                      <Box className={styles.svg_itm}>
+                        <AddToPhotosIcon />
+                      </Box>
+                      <h4>Anytime Transaction</h4>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Ut elit tellus, luctus nec ullamcorper mattis
+                      </p>
+                    </Box>
+                  </Grid>
+                  <Grid lg={6} md={12} sm={12}>
+                    <Box className={styles.items_sec}>
+                      <Box className={styles.svg_itm2}>
+                        <AddToPhotosIcon />
+                      </Box>
+                      <h4>Anytime Transaction</h4>
+                      <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                        Ut elit tellus, luctus nec ullamcorper mattis
+                      </p>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+              <Grid item lg={5} md={12} sm={12}>
+                <img
+                  src="https://advanture.icu/bden/wp-content/uploads/sites/18/2023/04/Dashboard.png"
+                  alt=""
+                />
+                {/* <Box className={styles.review_form}>
+                <h3>Read our blogs</h3>
+                </Box>
+                <Box className={styles.service_sec}>
+                    <div className={styles.service_item}>
+                    <img src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg" alt="" />
+                        <h5>Mobile Recharge & DTH</h5>
+                    </div>
+                    <div className={styles.service_item}>
+                    <img src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg" alt="" />
+                        <h5>Bill Payments</h5>
+                    </div>
+                    <div className={styles.service_item}>
+                    <img src="https://payrup.com/assets/images/Icons/mobile-recharge-dth.svg" alt="" />
+                        <h5>Travel Bookings</h5>
+                    </div>
+                </Box> */}
+              </Grid>
+            </Grid>
+          </Container>
+        </Box>
+      )}
+
+      <Box component={"section"} className={styles.home_wrp_third}>
+        <Container>
+          <Grid container spacing={3}>
+            <Grid item lg={5} sm={12} xs={12} md={12}>
+             
+                <Box className={styles.third_left_bag3}>
+                <h2>Everyone Should Have Access <span>To Financial Services</span></h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et</p>
+                </Box>
+           
+            </Grid>
+            <Grid item lg={7} sm={12} xs={12} md={12}>
+              <Box className={styles.heading_sec}>
+                <p>Our commitment to excellence is reflected in our customer service. We are always here to help you with any questions or concerns you may have. </p>
+                <button>Get Started</button>
+              </Box>
+            </Grid>
+          </Grid>
+        </Container>
+      </Box>
+    </>
+  );
+};
+
+export default Home;
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,149 @@
+"use client"
+import  React, { useEffect, useState } from "react";
+import PropTypes from "prop-types";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Container, Grid } from "@mui/material";
+const drawerWidth = 240;
+const navItems = ["Home", "About", "Contact"];
+import styles from "./Navbar.module.css"
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+function Navbar(props) {
+    const router = usePathname();
+    console.log(router);
+  const { window } = props;
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [scroll, setScroll] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  /**
+   *  Responsive Show side panel content
+   * NavItems
+   */
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <ListItemText primary={item} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+      
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+    // useEffect(() => {
+    //     const updateScroll = () => {
+    //       window.scrollY >= 90 ? setScroll(true) : setScroll(false);
+    //     };
+      
+    //     // Add the event listener
+    //     window.addEventListener("scroll", updateScroll);
+      
+    //     // Cleanup the event listener when the component is unmounted
+    //     return () => {
+    //       window.removeEventListener("scroll", updateScroll);
+    //     };
+    //   }, []);
+  return (
+    <Box className={styles.header_wrp} >
+      <AppBar component="nav">
+        <Toolbar className={styles.toolbar}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+
+          <Container>
+            <Grid container>
+              {/* Logo Here */}
+              <Grid item lg={3}>
+                <Box className={styles.logo_set} component={"div"} >
+                <h6>Zupay<sub>.com</sub></h6>
+                </Box>
+              </Grid>
+              {/* Listing Items for Linking */}
+              <Grid item lg={9} sx={{ display: { xs: "none", sm: "block" }}} >
+                <Box className={styles.list_wrp_ul} component={"ul"}>
+                  <Box className={styles.list_wrp_li} component={"li"}>
+                    <Link href={""}  className={router === "/" ? styles.active : ""} >Home</Link>
+                  </Box>
+                  <Box className={styles.list_wrp_li} component={"li"}>
+                    <Link  href={""}>Services</Link>
+                  </Box>
+                  <Box className={styles.list_wrp_li} component={"li"}>
+                    <Link  href={""}>About us</Link>
+                  </Box>
+                  <Box className={styles.list_wrp_li} component={"li"}>
+                    <Link  href={""}>Contact us</Link>
+                  </Box>
+                  <Box className="list_wrp_li" component={"li"}>
+                    <Link href={""} className={styles.loginBtn}>Login</Link>
+                  </Box>
+                  <Box className="list_wrp_li" component={"li"}>
+                    <Link href={""} className={styles.signupBtn} >Sign up</Link>
+                  </Box>
+                </Box>
+              </Grid>
+            </Grid>
+          </Container>
+        </Toolbar>
+      </AppBar>
+
+      {/* Responsive Show navbar sidepanel  */}
+      <nav>
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </nav>
+    </Box>
+  );
+}
+
+export default Navbar;
+// sx={{ display: { xs: "none", sm: "block" }, alignItems:"center", justifyContent:"flex-end" }} responsive hide list item
